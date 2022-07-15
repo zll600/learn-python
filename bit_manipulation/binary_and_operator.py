7fdeeb42b30262b41b293adc2c4ddd72c1e5ade5
zll600 <3400692417@qq.com>
2022-07-15 15:11:04 +0800

    Add Bit Manipulation and operation

---
+++ bit_manipulation/binary_and_operator.py
@@ -0,0 +1,54 @@
+def binary_and(a: int, b: int) -> str:
+    """
+    Take in 2 integers, convert them to binary,
+    return a binary number that is the
+    result of a binary and operation on the integers provided.
+    >>> binary_and(25, 32)
+    '0b000000'
+    >>> binary_and(37, 50)
+    '0b100000'
+    >>> binary_and(21, 30)
+    '0b10100'
+    >>> binary_and(58, 73)
+    '0b0001000'
+    >>> binary_and(0, 255)
+    '0b00000000'
+    >>> binary_and(256, 256)
+    '0b100000000'
+    >>> binary_and(0, -1)
+    Traceback (most recent call last):
+        ...
+    ValueError: the value of both inputs must be positive
+    >>> binary_and(0, 1.1)
+    Traceback (most recent call last):
+        ...
+    TypeError: 'float' object cannot be interpreted as an integer
+    >>> binary_and("0", "1")
+    Traceback (most recent call last):
+        ...
+    TypeError: '<' not supported between instances of 'str' and 'int'
+    """
+    
+    if a < 0 or b < 0:
+        raise ValueError("the value of both inputs must be positive")
+
+    # https://docs.python.org/3/library/functions.html?highlight=bin#bin
+    # Convert an integer number to a binary string prefixed with “0b”.
+    a_binary = str(bin(a))[2:]
+    b_binary = str(bin(b))[2:]
+
+    max_len = max(len(a_binary), len(b_binary))
+
+    # https://docs.python.org/3/library/stdtypes.html?highlight=zfill#str.zfill
+    # Return a copy of the string left filled with ASCII '0' digits to make a string of length width.
+    # https://docs.python.org/3/library/functions.html#zip
+    # Iterate over several iterables in parallel, producing tuples with an item from each one
+    return "0b" + "".join(
+                str(int(char_a == "1" and char_b == "1"))
+                for char_a, char_b in zip(a_binary.zfill(max_len), b_binary.zfill(max_len))
+            )
+
+if __name__=="__main__":
+    import doctest
+
+    doctest.testmod()
